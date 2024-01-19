9922432e133833f65e95ef0a9b46a9ae4fa76ef3
kubataba1uga <taba1uga6200@proton.me>
2024-01-19 21:17:19 +0100

    init repo

---
+++ provision_vm.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+VM_NAME=$1
+VM_USERNAME=taba1uga
+
+ansible-playbook --connection local -i $VM_NAME.lab,  -u taba1uga provision-rssh.yaml
+
+if [ $? -ne 0 ]; then
+    >&2 echo "ERROR ERROR ERROR"
+    >&2 echo "Provisioning rssh failed!!!"
+    >&2 echo "ERROR ERROR ERROR"
+    exit 1
+fi
+
