ad6c0b2ba9dc8b56083a32bcc4bf9fd21d86cba2
kubataba1uga <taba1uga6200@proton.me>
2024-01-20 03:52:39 +0100

    fix auth issues

--- provision_vm.sh
+++ provision_vm.sh
@@ -3,7 +3,7 @@
 VM_NAME=$1
 VM_USERNAME=taba1uga
 
-ansible-playbook -i $VM_NAME.lab,  -u taba1uga provision-rssh.yaml
+ansible-playbook -i $VM_NAME.lab, -u taba1uga provision-rssh.yaml 
 
 if [ $? -ne 0 ]; then
     >&2 echo "ERROR ERROR ERROR"
